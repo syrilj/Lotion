e350a449484637eccd04cb8b481e75df787537be
Syril Jacob <syriljacob@Syrils-MacBook-Pro.local>
2023-03-21 14:43:28 -0600

    change

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,27 +2,24 @@ import { Link } from "react-router-dom";
 import formatDate from "../hooks/formatDate";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.bubble.css";
+import {useNavigate } from "react-router-dom";
+
 const Title = ({title, time, id, mode, handleNoteSave, handleTitleChange, titleRef, handleTimeChange}) => {
+let navigate = useNavigate();
   
-  
-  const handleDelete = () => {
-    const answer = window.confirm("Are you sure?");
-    if (answer) {
-      const savedNotes = JSON.parse(localStorage.getItem('notes'));
-      const filteredNotes = savedNotes.filter((note, index) => index !== parseInt(id) - 1);
-      localStorage.setItem('notes', JSON.stringify(filteredNotes));
-      if(savedNotes.length === 1) {
-        window.location.href = '/notes';
-      }
-      else {
-        window.location.href = '/notes/1';
-      }
+const handleDelete = () => {
+  const answer = window.confirm("Are you sure?");
+  if (answer) {
+    const savedNotes = JSON.parse(localStorage.getItem('notes'));
+    const filteredNotes = savedNotes.filter((note, index) => index !== parseInt(id) - 1);
+    localStorage.setItem('notes', JSON.stringify(filteredNotes));
+    if (filteredNotes.length === 0) {
+      navigate('/notes');
+    } else {
+      navigate(`/notes/1`);
     }
   }
-
-
-
-
+}
   return (
     <>
       {mode === 'edit' ? 
@@ -47,7 +44,9 @@ const Title = ({title, time, id, mode, handleNoteSave, handleTitleChange, titleR
           <Link to={`/notes/${id}/edit`}>
             <button className="hover:bg-gray-200 py-3 px-7 border-2 rounded-lg">Edit</button>
           </Link>
+          <Link to ={'/notes'}>
           <button onClick={handleDelete} className="hover:bg-gray-200 py-3 px-7 border-2 rounded-lg">Delete</button>
+          </Link>
           </div>
       </div>
       }
