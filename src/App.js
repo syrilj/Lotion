f193dc763e7419dedc2a007da80150c2ae50b280
Syril Jacob <syriljacob@Syrils-MacBook-Pro.local>
2023-03-22 10:44:52 -0600

    fix for login

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Edit from "./pages/Edit";
 import Layout from "./pages/Layout";
 import LoginPage from "./pages/LoginPage";
 
-
 function App() {
   const [isLoggedInUser, setIsLoggedInUser] = useState(
     window.localStorage.getItem("isLoggedInUser")
@@ -15,21 +14,26 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage setIsLoggedInUser={setIsLoggedInUser} />} />
-        {/* Add a conditional check to prevent unauthorized access */}
-        {isLoggedInUser ? (
-          <Route element={<Layout />}>
-            <Route path="/" element={<Navigate to="/notes" />} />
-            <Route path="/notes" element={<Home />} />
-            <Route path="/notes/:id" element={<Home />} />
-            <Route path="/notes/:id/edit" element={<Edit />} />
-          </Route>
-        ) : (
-          <Route path="*" element={<Navigate to="/login" />} />
-        )}
+        <Route element={<Layout />}>
+          {isLoggedInUser ? (
+            <>
+              <Route path="/" element={<Navigate to="/notes" />} />
+              <Route path="/notes" element={<Home />} />
+              <Route path="/notes/:id" element={<Home />} />
+              <Route path="/notes/:id/edit" element={<Edit />} />
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<Navigate to="/login" />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </>
+          )}
+        </Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
 
