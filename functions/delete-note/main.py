4f29079e93afa7a7afaf83e1c30ddd1fe57a91a8
Syril Jacob <syriljacob@Syrils-MacBook-Pro.local>
2023-03-23 16:15:46 -0600

    fix for auth for lamda functions

--- functions/delete-note/main.py
+++ functions/delete-note/main.py
@@ -1,5 +1,6 @@
-# add your delete-note function here
 import boto3
+from google.oauth2 import id_token
+from google.auth.transport import requests
 
 # create a dynamodb resource
 dynamodb_resource = boto3.resource("dynamodb")
@@ -15,18 +16,29 @@ def delete_item(email, note_id):
         }
     )
 
-
-
 def lambda_handler(event, context):
 
     http_method = event["requestContext"]["http"]["method"].lower()
 
     if http_method == "delete":
+        # Get the access token from the request headers or body.
+        access_token = event["headers"]["Authorization"].split()[1]
 
-        email = event["queryStringParameters"]["email"]
+        # Verify the access token.
+        try:
+            idinfo = id_token.verify_oauth2_token(access_token, requests.Request(), "572348176466-51fd22tl7u7gcp26b0bcmrhj0rh08g3f.apps.googleusercontent.com")
+            # Extract the user's email address from the access token.
+            email = idinfo['email']
+        except ValueError:
+            # Invalid token
+            return {"statusCode": 401, "body": "Invalid token"}
+
+        # Get the note_id from the query parameters.
         note_id = event["queryStringParameters"]["note_id"]
-        
+
+        # Use the email address to delete the note from the DynamoDB table.
         delete_item(email, note_id)
+
     return {
         "statusCode": 200
     }
--- functions/get-notes/main.py
+++ functions/get-notes/main.py
@@ -1,6 +1,8 @@
-# add your get-notes function here
 import boto3
 from boto3.dynamodb.conditions import Key
+from google.oauth2 import id_token
+from google.auth.transport import requests
+
 # create a dynamodb resource
 dynamodb_resource = boto3.resource("dynamodb", region_name="ca-central-1")
 table_name = "lotion-30144227"
@@ -13,14 +15,23 @@ def lambda_handler(event, context):
     notes = None
 
     if http_method == "get":
-        # POST and PUT use the body to get info about the request
-        email = event["queryStringParameters"]["email"]
-        # read the docs: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodb/table/query.html
+        # Get the access token from the request headers or body.
+        access_token = event["headers"]["Authorization"].split()[1]
+
+        # Verify the access token.
+        try:
+            idinfo = id_token.verify_oauth2_token(access_token, requests.Request(), "572348176466-51fd22tl7u7gcp26b0bcmrhj0rh08g3f.apps.googleusercontent.com")
+            # Extract the user's email address from the access token.
+            email = idinfo['email']
+        except ValueError:
+            # Invalid token
+            return {"statusCode": 401, "body": "Invalid token"}
+
+        # Get the notes for the user.
         response = table.query(
-            TableName = table_name,
+            TableName=table_name,
             KeyConditionExpression=Key("email").eq(email)
         )
         notes = response["Items"]
-       
+
     return notes
-    
